0d123cf80d99774be63a248460de97060ac285ff
jeremycram <lungburner@gmail.com>
2017-02-02 10:39:44 -0700

    Add files via upload

--- lunch_2.rb
+++ lunch_2.rb
@@ -1,29 +1,64 @@
 require 'pry'
 
+@order = []
 @main = [
-  { dish:'meatloaf', price: 3.50 },
-  { dish: 'cheeseburger', price: 2.25 },
-  { dish:'katsu', price: 3.00 }
-]
+  { name:'meatloaf', price: 3.50 },
+  { name: 'cheeseburger', price: 2.25 },
+  { name:'katsu', price: 3.00 }]
+
+@side = [
+    { name: 'frys', price: 1.50},
+    { name: 'mashed potatoes', price: 1.00},
+    { name: 'chips', price: 0.15}]
+
 
 def lunch_lady
   puts 'pick a main dish'
-  @main.each do |dish|
-    puts "#{dish[:dish]} #{dish[:price]}"
-    input = gets.strip
+  @main.each_with_index do |dish, i|
+    puts "#{i + 1} #{dish[:name]} #{dish[:price]}"
+  end
+  input = gets.strip.to_i - 1
+  if input == -1
+    puts 'Invalid option'
+    lunch_lady
+  else
+    @order << @main[input]
+    side_menu
+  end
+end
 
+def side_menu
+  puts 'pick a side dish'
+  @side.each_with_index do |dish, i|
+    puts "#{i + 1} #{dish[:name]} #{dish[:price]}"
+  end
+  input = gets.strip.to_i - 1
+  if input == -1
+    puts 'Invalid option'
+    side_menu
+  else
+    @order << @side[input]
+  end
+  puts 'Would you like another side dish?'
+  input = gets.chomp.downcase
+  if input == 'y' || input == 'yes'
+    side_menu
+  else
+    print_order
+  end
+end
+
+def print_order
+  dishes = ''
+  price = 0.0
+  @order.each_with_index do |dish, i|
+    dishes += dish[:name]
+    dishes += ', ' unless i == @order.length - 1
+    price += dish[:price]
   end
 
+  puts "Your order: " + dishes
+  puts "Total: $#{price}"
 end
-
-@side = [
-  { dish: 'frys', price: 1.50}
-  { dish: 'mashed potatoes', price: 1.00}
-  { dish: 'chips', price: .15}
-
-end
-
-
-]
 
 lunch_lady
